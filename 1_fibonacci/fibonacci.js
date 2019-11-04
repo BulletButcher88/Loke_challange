6f4802cad72ff289583dc4bb88ab879158ca6469
Mark Butcher <mark@android-d35e89b71d7f8295.localdomain>
2019-11-04 17:47:01 +1100

    time of fucntion tested

--- 1_fibonacci/fibonacci.js
+++ 1_fibonacci/fibonacci.js
@@ -13,25 +13,31 @@
 
 // COMMENT: I found two approaches for a fibonacci function, an iterative solution and a reclusive solution. Despite the iterative function having more lines of code, it is quicker to call as a sequence Big O = O(n), where as the recursive solution Big O = O(n^2).
 
-// 1st solution: iterative 
-
-// function fibonacci(n) {
-//   let arr = [0 , 1]
-//   for( let x = 2; x <= n; x++){
-//     let a = arr[x - 1];
-//     let b = arr[x -2]
-//     arr.push(a + b);
-//   }
-//   return arr[n]
-// }
-
-// for (let i = 1; i < 10; i++) {
-//   console.log("index " + i, "=  " + fibonacci(i))
-// }
+// 1st solution: iterative processes in around 0.139ms
 
 
+console.time('fibonacci iterative')
 
-// 2nd solution: recursive 
+function fibonacci(n) {
+  let arr = [0 , 1]
+  for( let x = 2; x <= n; x++){
+    let a = arr[x - 1];
+    let b = arr[x -2]
+    arr.push(a + b);
+  }
+  return arr[n]
+}
+
+for (let i = 1; i < 10; i++) {
+  fibonacci(i)
+}
+
+console.timeEnd('fibonacci iterative')
+
+
+// 2nd solution: recursive processes in around 0.123ms
+
+// console.time('fibonacci recursive')
 
 // function fibonacci(n) {
 //   if (n < 2) {
@@ -41,13 +47,31 @@
 // }
 
 // for (let i = 1; i < 10; i++) {
-//   console.log("index " + i, "=  " + fibonacci(i))
+//   fibonacci(i)
 // }
 
+// console.timeEnd('fibonacci recursive')
+
+
+// 3rd solution: while loop processes in around 0.117ms
 
 
 
+// console.time('fibonacci loop')
 
+// function fibonacci(num){
+//   let a = 1, b = 0, temp;
 
+//   while (num >= 0){
+//     temp = a;
+//     a = a + b;
+//     b = temp;
+//     num--;
+//   }
+// return b
 
+// }
 
+// fibonacci(8)
+
+// console.timeEnd('fibonacci loop')
